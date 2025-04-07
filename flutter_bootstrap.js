5fadf71f9a4e6c09614a7b74b716e85a5ca5722b
willskymaker <willskymaker@users.noreply.github.com>
2025-04-07 12:14:34 +0000

    deploy: d0abf6f37a564888259ac23c8d6aa2e74e2ff532

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1264074996"
+    serviceWorkerVersion: "2965427442"
   }
 });
