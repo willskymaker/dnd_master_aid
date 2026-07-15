9fc065f75e6007dcfd3c1cd338777a62592d66ad
willskymaker <willskymaker@users.noreply.github.com>
2026-07-15 09:20:13 +0000

    deploy: 150867f7121c9ac066cf718c4a3bb821819ff329

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1054802108" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2558202165" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
